21e60d2242eeabaaf93bd589e33df5f007883726
bebiksik <bebiks.priv@gmail.com>
2023-05-03 00:06:08 +0200

    status-codes flag
    (made with chatgpt 👍)
    
    I'm really bad at golang :(

--- main.go
+++ main.go
@@ -5,8 +5,6 @@ import (
 	"crypto/tls"
 	"flag"
 	"fmt"
-	"io"
-	"io/ioutil"
 	"net"
 	"net/http"
 	"os"
@@ -47,6 +45,10 @@ func main() {
 	// prefer https
 	var preferHTTPS bool
 	flag.BoolVar(&preferHTTPS, "prefer-https", false, "only try plain HTTP if HTTPS fails")
+
+	// show status codes
+	var showStatusCodes bool
+	flag.BoolVar(&showStatusCodes, "status-codes", false, "show status codes in output")
 
 	// HTTP method to use
 	var method string
@@ -96,8 +98,13 @@ func main() {
 
 				// always try HTTPS first
 				withProto := "https://" + url
-				if isListening(client, withProto, method) {
-					output <- withProto
+				if code, ok := isListening(client, withProto, method); ok {
+					if showStatusCodes {
+						output <- fmt.Sprintf("%d %s", code, withProto)
+					} else {
+						output <- withProto
+					}
+
 
 					// skip trying HTTP if --prefer-https is set
 					if preferHTTPS {
@@ -120,8 +127,12 @@ func main() {
 		go func() {
 			for url := range httpURLs {
 				withProto := "http://" + url
-				if isListening(client, withProto, method) {
-					output <- withProto
+				if code, ok := isListening(client, withProto, method); ok {
+					if showStatusCodes {
+						output <- fmt.Sprintf("%d %s", code, withProto)
+					} else {
+						output <- withProto
+					}
 					continue
 				}
 			}
@@ -210,25 +221,17 @@ func main() {
 	outputWG.Wait()
 }
 
-func isListening(client *http.Client, url, method string) bool {
-
+func isListening(client *http.Client, url string, method string) (int, bool) {
 	req, err := http.NewRequest(method, url, nil)
 	if err != nil {
-		return false
+		return 0, false
 	}
-
-	req.Header.Add("Connection", "close")
-	req.Close = true
 
 	resp, err := client.Do(req)
-	if resp != nil {
-		io.Copy(ioutil.Discard, resp.Body)
+	if err == nil {
 		resp.Body.Close()
+		return resp.StatusCode, true
 	}
 
-	if err != nil {
-		return false
-	}
-
-	return true
+	return 0, false
 }
